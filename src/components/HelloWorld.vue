16c078dc90393e5f012919a878c11d6ab87698e5
许大仙 <1900919313@qq.com>
2022-09-20 20:24:34 +0800

    ✨

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    你好啊
-  </div>
+  <div>当前n的值为：{{n}}</div>
+  <button @click="n++">点我+1</button>
 </template>
 
 <script lang="ts" setup>
-
+import { ref } from 'vue'
+let n = ref(0)
 </script>
 
 <style lang="scss">
