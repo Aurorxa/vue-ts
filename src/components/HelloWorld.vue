1e26582ac2844a01673a856232b70d85f2e514a5
许大仙 <1900919313@qq.com>
2022-09-21 12:59:46 +0800

    ✨

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,40 @@
 <template>
   <div>当前n的值为：{{n}}</div>
-  <button @click="n++">点我+1</button>
+  <button @click="add">点我+1</button>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-let n = ref(0)
+<script lang="tsx" setup>
+import { ref, reactive } from 'vue'
+let n = ref<number>(0)
+
+// 增加方法
+const add = () => {
+  n.value++
+}
+
+type ResultType = {
+  code: string,
+  msg: string,
+  data: {a:number,b:number}[]
+}
+
+let p: Promise<ResultType> = new Promise<ResultType>((resolve, reject) => {
+  resolve({
+    code: '200',
+    msg: '',
+    data: [
+      { a: 100, b: 100 }
+    ]
+  })
+})
+
+p.then(res => {
+  console.log(res.code);
+  console.log(res.data);
+  console.log(res.msg);
+})
+
+
 </script>
 
 <style lang="scss">
