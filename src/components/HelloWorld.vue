3cd503d94bd5a5e53af1974c753841e7ccb9d49c
许大仙 <1900919313@qq.com>
2022-09-21 17:13:58 +0800

    🐛

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,25 @@
 <template>
   <div>当前n的值为：{{n}}</div>
   <button @click="add">点我+1</button>
+  <ul>
+    <li>学生姓名：{{student.name}}</li>
+    <li>学生年龄：{{student.age}}</li>
+  </ul>
 </template>
 
 <script lang="tsx" setup>
 import { ref, Ref } from 'vue'
-let n: Ref<number> = ref<number>(0)
+const n: Ref<number> = ref<number>(0)
+
+type StudentType = {
+  name: string,
+  age: number
+}
+
+const student: Ref<StudentType> = ref<StudentType>({
+  name: '许大仙',
+  age: 18
+})
 
 // 增加方法
 const add = () => {
