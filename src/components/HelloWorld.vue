ca4877f3898c8c28d99ab648b1f9a24775231591
许大仙 <1900919313@qq.com>
2022-09-21 21:26:31 +0800

    💄

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,20 @@
 <template>
   <div>当前n的值为：{{n}}</div>
   <button @click="add">点我+1</button>
+  <h2>学生信息</h2>
   <ul>
     <li>学生姓名：{{student.name}}</li>
     <li>学生年龄：{{student.age}}</li>
   </ul>
+  <h2>学校信息</h2>
+  <ul>
+    <li>学校名称：{{school.name}}</li>
+    <li>学校地址：{{school.address}}</li>
+  </ul>
 </template>
 
 <script lang="tsx" setup>
-import { ref, Ref } from 'vue'
+import { reactive, ref, Ref } from 'vue'
 const n: Ref<number> = ref<number>(0)
 
 type StudentType = {
@@ -19,6 +25,16 @@ type StudentType = {
 const student: Ref<Partial<StudentType>> = ref<Partial<StudentType>>({
   name: '许大仙',
   age: 18
+})
+
+type SchoolType = {
+  name: string,
+  address: string
+}
+
+const school = reactive<SchoolType>({
+  name: '江苏苏州大学',
+  address: '江苏苏州'
 })
 
 // 增加方法
