be303e74c7f57bd65a0b67931bb1ff38d336b0bc
许大仙 <1900919313@qq.com>
2022-09-22 11:11:47 +0800

    ✨

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-
+    <el-config-provider :locale="zhCn" :z-index="10000">
+        <router-view />
+    </el-config-provider>
 </template>
 
-<script setup lang="ts">
-
-
+<script setup lang="tsx">
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 </script>
 
 <style scoped>
